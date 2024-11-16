1a36002089706d053d0158939cdbcb7ded5d072e
Tahseer <89750321+Tahseer@users.noreply.github.com>
2024-11-16 15:43:50 +0500

    Add files via upload

---
+++ lib/theme_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ThemeColors {
+  static const Color green =
+      Color.fromARGB(255, 201, 243, 87); // Fresh green for buttons (updated)
+  static const Color red = Color(0xFFF1628C); // Keeping the original red
+  static const Color yellow =
+      Color.fromARGB(255, 137, 13, 186); // Keeping the original yellow
+}
